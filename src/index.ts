f8b50d655be1087c9b331dcdc30f99a2f5437473
Samuel Pires <samuel.ferpires@gmail.com>
2022-12-21 00:51:58 -0300

    testing sample routes with and without input

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,49 @@
-import dotenv from 'dotenv-safe';
-
+import { createConfig, z, Routing, defaultEndpointsFactory, createServer } from "express-zod-api";
+import dotenv from "dotenv-safe";
 dotenv.config();
 
-console.log('Node js boilerplate is running')
+const config = createConfig({
+  server: {
+    listen: 3000,
+  },
+  cors: true,
+  logger: { level: "debug", color: true },
+});
+
+const primaryEndPoint = defaultEndpointsFactory.build({
+  method: "get",
+  input: z.object({}),
+  output: z.object({
+    gretting: z.string(),
+  }),
+  handler: async () => {
+    return {
+      gretting: "Hahahaha",
+    };
+  },
+});
+
+const needsInput = defaultEndpointsFactory.build({
+  method: "post",
+  input: z.object({
+    sampleInput: z.string().min(6)
+  }),
+  output: z.object({
+    validation: z.string()
+  }),
+  handler: async ({input, logger}) => {
+    logger.info("SUCCESS:",input)
+    return {
+      validation: "Input successully validated"
+    }
+  }
+})
+
+const routes: Routing = {
+  api: {
+    hello: primaryEndPoint,
+    world: needsInput
+  },
+};
+
+createServer(config, routes);
